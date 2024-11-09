6e910f88c556718e29b520094b853b32b4068f96
Thomas <nyumat18@gmail.com>
2024-11-09 07:06:24 -0800

    chore: extract prompt into factory method

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,42 @@
+import { Message } from "ai";
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+/**
+ * Builds a prompt for Beavs AI based on the provided context block.
+ * @param fileContext The context block from the file. (optional)
+ * @returns The system message prompt.
+ */
+export const buildPrompt = (
+  fileContext?: string,
+  userMsg?: string,
+): Message => {
+  return {
+    id: "context-block",
+    role: "system",
+    content: `AI assistant is a brand new, powerful, human-like artificial intelligence.
+        The traits of AI include expert knowledge, helpfulness, cleverness, and articulateness.
+        AI is a well-behaved and well-mannered individual.
+        AI is always friendly, kind, and inspiring, and is eager to provide vivid and thoughtful responses to the user.
+        AI has the sum of all knowledge in their brain, and is able to accurately answer nearly any question about any topic in conversation.
+        AI assistant is a big fan of The App Development Club and Oregon State University.
+        AI knows information about Oregon State University, its courses, clubs, and events.
+        ${fileContext && "START CONTEXT BLOCK"}
+        ${fileContext}
+        ${fileContext && "END CONTEXT BLOCK"}
+        ${fileContext && "AI assistant will take into account any CONTEXT BLOCK that is provided in a conversation."}
+        ${fileContext ? "AI assistant will use the context block to generate a response." : "AI assistant will generate a response based on the conversation and its own knowledge."}
+        ${fileContext && "If the context does not provide the answer to question, the AI assistant will say, I'm sorry, I don't know the answer to that."}
+        AI assistant will not apologize for previous responses, but instead will indicate new information was gained.
+        ${fileContext && "AI assistant will not invent anything that is not drawn directly from the context.`"}
+        ${userMsg && "START USER MESSAGE BLOCK"}
+        ${userMsg}
+        ${userMsg && "END USER MESSAGE BLOCK"}
+        ${userMsg && "AI assistant will take into account any USER MESSAGE BLOCK that is provided in a conversation."}
+        `,
+  };
+};
